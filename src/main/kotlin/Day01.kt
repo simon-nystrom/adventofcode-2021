4def5930727e70851b7833d4f2c9cb3d93132084
Simon Nyström <simon.nystrom90@gmail.com>
2021-12-01 06:19:56 +0100

    day 1 completed

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,21 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+
+fun part1(): Int {
+    return Path("inputs/day01.txt").readLines().map { it.toInt() }
+        .windowed(2)
+        .fold(0) { acc, window -> if (window[1] > window[0]) acc + 1 else acc }
+}
+
+fun part2(): Int {
+    return Path("inputs/day01.txt").readLines().map { it.toInt() }
+        .windowed(3)
+        .windowed(2)
+        .fold(0) { acc, windows -> if (windows[1].sum() > windows[0].sum()) acc + 1 else acc }
+}
+
+fun main(args: Array<String>) {
+    println(part1())
+    println(part2())
+}
