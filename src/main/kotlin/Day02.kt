ad9dc25d6f9b6db9227bb95454bc735e1a5120f0
Simon Nyström <simon.nystrom90@gmail.com>
2021-12-01 06:33:25 +0100

    day 2 prep

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,20 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day02 {
+    companion object {
+        fun part1(): Any {
+            return Path("inputs/day02.txt").readLines()
+        }
+
+        fun part2(): Any {
+            return Path("inputs/day02.txt").readLines()
+        }
+    }
+}
+
+
+fun main(args: Array<String>) {
+    println(Day02.part1())
+    println(Day02.part2())
+}
