478c93eb9b27e72b873c0b40ffc735a42a6d8f8d
Simon Nyström <simon.nystrom90@gmail.com>
2021-12-02 06:37:43 +0100

    day 2 complete

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,11 +4,41 @@ import kotlin.io.path.readLines
 class Day02 {
     companion object {
         fun part1(): Any {
-            return Path("inputs/day02.txt").readLines()
+            val mapped =
+                Path("inputs/day02.txt").readLines()
+                    .map { it.split(" ") }
+                    .groupBy({ it.first() }, { it.last().toInt() })
+
+            return mapped["forward"]!!.sum() * (mapped["down"]!!.sum() - mapped["up"]!!.sum())
+
         }
 
         fun part2(): Any {
-            return Path("inputs/day02.txt").readLines()
+
+            val submarine = object {
+                var aim = 0
+                var dist = 0
+                var depth = 0
+            }
+
+            val mapped =
+                Path("inputs/day02.txt").readLines()
+                    .map { it.split(" ") }
+                    .fold(submarine) { acc, list ->
+                        val num = list.last().toInt()
+                        when (list.first()) {
+                            "forward" -> {
+                                acc.dist += num
+                                acc.depth += acc.aim * num
+                            }
+                            "up" -> acc.aim -= num
+                            "down" -> acc.aim += num
+                        }
+                        acc
+                    }
+
+            return mapped.depth * mapped.dist
+
         }
     }
 }
